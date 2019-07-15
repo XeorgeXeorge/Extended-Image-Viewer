9d78aac17d8a0772a0ea3ff36f8da72ec98c9911
George Mns <georgeminatsis@outlook.com>
2019-07-15 23:02:33 +0300

    initial Phase 2

--- ImageViewer_Phase_2/ImageControl.xaml.cs
+++ ImageViewer_Phase_2/ImageControl.xaml.cs
@@ -1,15 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Threading.Tasks;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-namespace ImageViewer_Phase_1
-{
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
 
-    public sealed partial class ImagePage : Page
+namespace ImageViewer_Phase_2
+{
+    public sealed partial class ImageControl : UserControl
     {
 
         public ScrollBarVisibility FitImageVertically
@@ -19,7 +27,7 @@ namespace ImageViewer_Phase_1
         }
 
         public static readonly DependencyProperty FitImageVerticallyProperty =
-            DependencyProperty.Register("FitImageVertically", typeof(ScrollBarVisibility), typeof(ImagePage), new PropertyMetadata(ScrollBarVisibility.Visible));
+            DependencyProperty.Register("FitImageVertically", typeof(ScrollBarVisibility), typeof(ImageControl), new PropertyMetadata(ScrollBarVisibility.Disabled));
 
 
         public ScrollBarVisibility FitImageHorizontally
@@ -29,62 +37,12 @@ namespace ImageViewer_Phase_1
         }
 
         public static readonly DependencyProperty FitImageHorizontallyProperty =
-            DependencyProperty.Register("FitImageHorizontally", typeof(ScrollBarVisibility), typeof(ImagePage), new PropertyMetadata(ScrollBarVisibility.Visible));
-         
+            DependencyProperty.Register("FitImageHorizontally", typeof(ScrollBarVisibility), typeof(ImageControl), new PropertyMetadata(ScrollBarVisibility.Disabled));
 
-        List<BitmapImage> StoredImages = new List<BitmapImage>();
-
-        int CurrentImageIndex = -1;
-
-
-        public ImagePage()
+        public ImageControl()
         {
             this.InitializeComponent();
         }
-
-
-        protected override async void OnNavigatedTo(NavigationEventArgs e)
-        {
-            base.OnNavigatedTo(e);
-
-
-            await InitialLoad();
-        }
-
-
-        async Task InitialLoad()
-        {
-            var dummyfolder = await Windows.ApplicationModel.Package.Current.InstalledLocation.GetFolderAsync(@"ImageProvider\Dummies");
-
-
-
-            int indx = 0;
-            foreach (var fs in await dummyfolder.GetFilesAsync())
-            {
-                StoredImages.Add( new BitmapImage(new Uri(fs.Path)));
-                indx++;
-            }
-
-            //initial image set
-            CurrentImageIndex = 0;
-            CoreImage.Source = StoredImages[0];
-        }
-
-
-
-
-
-
-
-
-        private void ChangeImageButton_Tapped(object sender, Windows.UI.Xaml.Input.TappedRoutedEventArgs e)
-        {
-            CurrentImageIndex = (CurrentImageIndex + 1) % StoredImages.Count;//cicular incrementing
-            CoreImage.Source = StoredImages[CurrentImageIndex];
-
-            ZoomAndPanningScrollViewer.ChangeView(0, 0, 1);//reset zoom
-        }
-
 
 
         private void LockVerticallyToggleButton_Click(object sender, RoutedEventArgs e)
@@ -103,9 +61,8 @@ namespace ImageViewer_Phase_1
             ZoomAndPanningScrollViewer.ChangeView(0, 0, 1);
         }
 
-
         private void ZoomInButton_Tapped(object sender, Windows.UI.Xaml.Input.TappedRoutedEventArgs e)
-        {          
+        {
             var ZoomAmount = ZoomAndPanningScrollViewer.ZoomFactor + 0.15f;
             ZoomAndPanningScrollViewer.ChangeView(null, null, ZoomAmount);
         }
@@ -114,8 +71,7 @@ namespace ImageViewer_Phase_1
         {
             var ZoomAmount = ZoomAndPanningScrollViewer.ZoomFactor - 0.15f;
             ZoomAndPanningScrollViewer.ChangeView(null, null, ZoomAmount);
+        
         }
-
-
     }
 }
---
+++ ImageViewer_Phase_2/ViewerPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+
+namespace ImageViewer_Phase_2
+{ 
+    public sealed partial class ViewerPage : Page
+    {
+        public ViewerPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+
+            await InitialLoad();
+        }
+
+
+        async Task InitialLoad()
+        {
+            var dummyfolder = await Windows.ApplicationModel.Package.Current.InstalledLocation.GetFolderAsync(@"ImageProvider\Dummies");
+            
+            foreach (var fs in await dummyfolder.GetFilesAsync())
+            {
+                TheFlipView.Items.Add(new BitmapImage(new Uri(fs.Path)));
+                
+            } 
+        }
+
+    }
+}
